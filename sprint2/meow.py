6846a877d485f8d7a4b8d3dc275d56e06ff5ed6f
Gritsaev Sergei <sergey_gritsaev@mail.ru>
2024-04-16 16:20:51 +0300

    sprint2

---
+++ sprint2/meow.py
@@ -0,0 +1,7 @@
+def add(xy, yy):
+    return x + y
+
+
+element_x = 10
+element_y = 5
+print("Сумма чисел: ", add(element_x, element_y))
---
+++ sprint2/ttest.py
@@ -0,0 +1,30 @@
+from math import sqrt
+from typing import Optional, Union
+
+
+# pep8-naming: проверяет именование классов, функций и переменных в 
+# соответствии с PEP 8;
+# flake8-isort: проверяет корректность последовательности импортов
+
+def add_numbers(first: int, second: int) -> int:
+    return first + second
+
+
+def calculate_square_root(number: Union[float, int]) -> float:
+    return sqrt(number)
+
+
+def calc(your_number: Union[int, float]) -> Optional[str]:
+    if your_number <= 0:
+        return None
+    root = calculate_square_root(your_number)
+    return ('Мы вычислили квадратный корень из введённого вами числа. '
+            f'Это будет: {root}')
+
+
+first: int = 10
+second: int = 5
+
+print('Сумма чисел: ', add_numbers(first, second))
+
+print(calc(25.5))
