294c1262815426cd84be3d68f3b609be8c67a499
Gritsaev Sergei <sergey_gritsaev@mail.ru>
2024-04-17 17:18:28 +0300

    Add sprint3 edu_files and tested venv with py 3.11

--- sprint3/employee.py
+++ sprint3/employee.py
@@ -1,1 +1,36 @@
-pass
+class Employee:
+    vacation_days = 28
+    remaining_vacation_days = vacation_days
+
+    def __init__(self, first_name, second_name, gender) -> None:
+        self.first_name = first_name
+        self.second_name = second_name
+        self.gender = gender
+        self.remaining_vacation_days = self.vacation_days
+
+    def consume_vacation(self, days):
+        self.remaining_vacation_days -= days
+
+    def get_vacation_details(self):
+        return f"Остаток отпускных дней: {self.remaining_vacation_days}."
+
+
+class FullTimeEmployee(Employee):
+    def get_unpaid_vacation(self, str_date, days):
+        return f"Начало неоплачиваемого отпуска: {str_date}, продолжительность: {days} дней."
+
+
+class PartTimeEmployee(Employee):
+    vacation_days = 14
+    remaining_vacation_days = vacation_days
+
+    def __init__(self, first_name, second_name, gender) -> None:
+        super().__init__(first_name, second_name, gender)
+
+
+# Пример использования:
+full_time_employee = FullTimeEmployee("Роберт", "Крузо", "м")
+print(full_time_employee.get_unpaid_vacation("2023-07-01", 5))
+part_time_employee = PartTimeEmployee("Алёна", "Пятницкая", "ж")
+print(part_time_employee.get_vacation_details())
+print(part_time_employee.remaining_vacation_days)
---
+++ sprint3/tic_tac_toe/game.py
@@ -0,0 +1,17 @@
+# game.py
+
+from gameparts import Board
+
+
+# Вот новая функция.
+def main():
+    game = Board()
+    game.display()
+    game.make_move(1, 1, "X")
+    print("Ход сделан!")
+    game.display()
+
+
+# А вот вызов этой функции.
+if __name__ == "__main__":
+    main()
