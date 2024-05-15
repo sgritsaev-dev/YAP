e69f881065893a038df054b5ae7144bed6b1e76f
Sergei Gritsaev (Home) <sergey_gritsaev@mail.ru>
2024-05-15 07:16:42 +0300

    delivery check v2

--- projects/delivery/platform_counter.py
+++ projects/delivery/platform_counter.py
@@ -1,7 +1,16 @@
 '''ID решения 114030549'''
 
 
-def check_platforms(sorted_robots: list, limit: int) -> int:
+def count_platforms(sorted_robots: list, limit: int) -> int:
+    """
+    A function which takes a sorted list of robots weights and an weight
+    limit one platform can take, returns amount of platforms
+    needed to carry all robots. For example,
+    >>> count_platforms([1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 6, 6, 6], 7)
+    7
+    >>> count_platforms([1, 1, 2, 2, 3, 4, 5, 6, 8, 10, 10, 10, 11], 11)
+    8
+    """
     counter = 0
     left_flag = 0
     right_flag = len(sorted_robots)-1
@@ -20,6 +29,8 @@ def check_platforms(sorted_robots: list, limit: int) -> int:
 
 
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
     robots = [int(robot_weight) for robot_weight in input().split()]
     limit = int(input())
-    print(check_platforms(sorted(robots), limit))
+    print(count_platforms(sorted(robots), limit))
