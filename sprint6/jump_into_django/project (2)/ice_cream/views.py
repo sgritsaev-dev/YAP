ec00e8afc5036e63dba57d6c037454950d762597
Sergei Gritsaev <sergey_gritsaev@mail.ru>
2024-06-18 17:04:48 +0300

    add django project

---
+++ sprint6/jump_into_django/project (2)/ice_cream/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+ICE_CREAM = chr(127846)
+
+
+def ice_cream_detail(request, pk):
+    return HttpResponse(f'Мороженое номер {pk}')
+
+
+def ice_cream_list(request):
+    return HttpResponse(f'Каталог мороженого')
