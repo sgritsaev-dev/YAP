632c33d309d7e0d6f2eb13f5dbd36bb5e0f4f1e7
Sergei Gritsaev <sergey_gritsaev@mail.ru>
2024-07-04 17:25:14 +0300

    django orm day 3

---
+++ sprint8/project (17)/homepage/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from ice_cream.models import IceCream
+
+
+def index(request):
+    template_name = "homepage/index.html"
+    ice_cream_list = (
+        IceCream.objects.values('wrapper__title', "id", "title", "description")
+        .filter(is_published__exact=True, is_on_main__exact=True)
+        .order_by("title")[0:3]
+    )
+    context = {
+        "ice_cream_list": ice_cream_list,
+    }
+    return render(request, template_name, context)
---
+++ sprint8/project (17)/ice_cream/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import get_object_or_404, render
+
+from ice_cream.models import IceCream
+
+
+def ice_cream_detail(request, pk):
+    template_name = 'ice_cream/detail.html'
+    ice_cream = get_object_or_404(
+        IceCream.objects.values(
+            'title', 'description'
+        ).filter(is_published=True),
+        pk=pk
+    )
+    context = {
+        'ice_cream': ice_cream,
+    }
+    return render(request, template_name, context)
+
+
+def ice_cream_list(request):
+    template = 'ice_cream/list.html'
+    context = {}
+    return render(request, template, context)
